c49523d5048b4a63ce376a993f7d76b0b29044be
gyhmac <gyeonho@gmail.com>
2026-01-10 14:46:32 +0900

    Initial Commit

---
+++ netflix-clone-github/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  netflix-clone-github
+//
+//  Created by gyhmac on 1/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ netflix-clone-github/netflix_clone_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflix_clone_githubApp.swift
+//  netflix-clone-github
+//
+//  Created by gyhmac on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct netflix_clone_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
