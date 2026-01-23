543e4537fa104c1c51054ea1f5010c9e2a42c463
gyhcom <59407250+gyhcom@users.noreply.github.com>
2026-01-23 15:50:16 +0900

    FEAT:Sign in #1

--- netflix-clone-github/Core/Auth/Onboarding/SignIn/View/SignIn.swift
+++ netflix-clone-github/Core/Auth/Onboarding/SignIn/View/SignIn.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SignIn: View {
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var showPassword: Bool = false
     
     var body: some View {
         ZStack{
@@ -30,6 +31,61 @@ struct SignIn: View {
                 .background(.appDarkGray)
                 .foregroundStyle(.white)
                 .cornerRadius(4)
+                
+                if showPassword {
+                    TextField(
+                      "",
+                      text: $email,
+                      prompt: Text("Email or phone number")
+                        .foregroundStyle(.white)
+                    )
+                    .overlay {
+                        VStack{
+                            Button{
+                                showPassword = false
+                            } label: {
+                                Image(systemName: "eye.slash")
+                            }
+                            
+                        }
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    .keyboardType(.emailAddress)
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .padding()
+                    .background(.appDarkGray)
+                    .foregroundStyle(.white)
+                    .cornerRadius(4)
+                } else {
+                    SecureField(
+                        "",
+                        text: $password,
+                        prompt: Text("Password").foregroundStyle(.white)
+                    )
+                    .overlay {
+                        VStack{
+                            Button{
+                                showPassword = true
+                            } label: {
+                                Image(systemName: "eye")
+                            }
+                            
+                        }
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .padding()
+                    .background(.appDarkGray)
+                    .foregroundStyle(.white)
+                    .cornerRadius(4)
+                }
+                
+                
             }
         }
     }
